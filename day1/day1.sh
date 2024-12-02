8f843014a5888b767e78c5bf99dddb045300ab85
Andreas Gebert <47990291+cfxxldev@users.noreply.github.com>
2024-12-02 19:49:02 +0100

    first commit

---
+++ day1/day1.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+
+liste1=($(cat input | awk '{print $1}' | sort))
+liste2=($(cat input | awk '{print $2}' | sort))
+
+part1() {
+    gesamt=0
+    for i in "${!liste1[@]}"; do
+        diff=$(awk "BEGIN {x=${liste1[$i]}; y=${liste2[$i]}; print y-x;}")
+        diff=${diff/-/}
+        gesamt=$(awk "BEGIN {x=${gesamt}; y=${diff}; print x+y;}")
+    done
+    echo "$gesamt"
+}
+
+part2() {
+    gesamt=0
+    for n in "${liste1[@]}"; do
+        count=$(echo "${liste2[@]}" | tr " " "\n" | grep -c "${n}")
+        gesamt=$(awk "BEGIN {x=${gesamt}; y=(${n} * ${count}); print x+y;}")
+    done
+    echo "$gesamt"
+}
+
+part1
+part2
