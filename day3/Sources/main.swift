3d1eb0f10c6deaef033bece5489936d633bebef0
Andreas Gebert <47990291+cfxxldev@users.noreply.github.com>
2024-12-05 04:13:11 +0100

    updated day4

--- day3/Sources/main.swift
+++ day3/Sources/main.swift
@@ -11,23 +11,22 @@ func read_file() -> String {
     }
 }
 
-func part1() {
+func part1() -> Int {
     let content = read_file()
     let regex = /(mul)\((\d{1,3}),(\d{1,3})\)/
 
-    let summe = content.matches(of: regex).reduce(0) { partialResult, m in
+    return content.matches(of: regex).reduce(0) { partialResult, m in
         partialResult + ((Int(m.output.2) ?? 0) * (Int(m.output.3) ?? 0))
     }
-    print(summe)
 }
 
-func part2() {
+func part2() -> Int {
     let content = read_file()
     let regex: Regex<(Substring, Substring, Substring?, Substring?)> =
         /(?:(do|don't|mul))\((?:(?:)|(\d+)\,(\d+))\)/
 
     var mul = 1
-    let summe = content.matches(of: regex).compactMap { m in
+    return content.matches(of: regex).compactMap { m in
         switch m.output {
         case (_, "do", nil, nil):
             mul = 1
@@ -36,18 +35,19 @@ func part2() {
             mul = 0
             return 0
         case (_, "mul", .some(let param1), .some(let param2)):
-            return [param1, param2].compactMap { Int($0) }.reduce(mul) {
-                sum, value in sum * value
+            if case (.some(let p1), .some(let p2)) = (Int(param1), Int(param2)) {
+                return [p1, p2].reduce(mul) {
+                    sum, value in sum * value
+                }
             }
+            return 0
         default:
             return 0
         }
     }.reduce(0) { partialResult, value in
         partialResult + value
     }
-
-    print(summe)
 }
 
-part1()
-part2()
+print("part1: \(part1())")
+print("part2: \(part2())")
