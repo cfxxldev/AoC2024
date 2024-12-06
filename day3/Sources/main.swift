e4146882c043ea1dd51dbe6fede366bdc50a1956
Andreas Gebert <47990291+cfxxldev@users.noreply.github.com>
2024-12-06 03:35:35 +0100

    updated day3

--- day3/Sources/main.swift
+++ day3/Sources/main.swift
@@ -1,4 +1,5 @@
 import Foundation
+import RegexBuilder
 
 func read_file() -> String {
   do {
@@ -20,27 +21,48 @@ func part1() -> Int {
   }.reduce(0, +)
 }
 
+enum Operation: Comparable {
+  case doIt
+  case dont
+  case mul(x: Int, y: Int)
+}
+
 func part2() -> Int {
   let content = read_file()
-  let regex: Regex<(Substring, Substring, Substring?, Substring?)> =
-    /(?:(do|don't|mul))\((?:(?:)|(\d+)\,(\d+))\)/
 
+  let regDo = /do\(\)/
+  let regDont = /don't\(\)/
+  let regMul = /mul\(\d{1,3},\d{1,3}\)/
+  let regMul_Capture = /mul\((\d{1,3}),(\d{1,3})\)/
+
+  let regex = Regex {
+    TryCapture {
+      ChoiceOf {
+        regDo
+        regDont
+        regMul
+      }
+    } transform: { (t) -> Operation in
+      if let d = t.wholeMatch(of: regMul_Capture) {
+        return .mul(x: Int(d.output.1) ?? 0, y: Int(d.output.2) ?? 0)
+      } else if t.wholeMatch(of: regDont) != nil {
+        return .dont
+      } else {
+        return .doIt
+      }
+    }
+  }
   var mul = 1
   return content.matches(of: regex).compactMap { m in
-    switch m.output {
-    case (_, "do", nil, nil):
+    switch m.output.1 {
+    case .doIt:
       mul = 1
       return 0
-    case (_, "don't", nil, nil):
+    case .dont:
       mul = 0
       return 0
-    case (_, "mul", .some(let param1), .some(let param2)):
-      if case (.some(let p1), .some(let p2)) = (Int(param1), Int(param2)) {
-        return mul * p1 * p2
-      }
-      return 0
-    default:
-      return 0
+    case let .mul(param1, param2):
+      return mul * param1 * param2
     }
   }.reduce(0, +)
 }
