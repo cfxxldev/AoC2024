1f20bb3a89635b451cd025d38d59e88dda9fee92
Andreas Gebert <47990291+cfxxldev@users.noreply.github.com>
2024-12-11 04:48:35 +0100

    updated day9

--- day9/Package.swift
+++ day9/Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "day9",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "day9"),
-    ]
+  name: "day9",
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "day9")
+  ]
 )
