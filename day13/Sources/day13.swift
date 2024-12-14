ea9abde35839236ca06a3749529b66399d9a5bb0
Andreas Gebert <47990291+cfxxldev@users.noreply.github.com>
2024-12-14 01:22:34 +0100

    updated day13

--- day13/Sources/day13.swift
+++ day13/Sources/day13.swift
@@ -1,5 +1,3 @@
-import RegexBuilder
-
 typealias Vector3D = SIMD3<Int64>
 struct Block {
   var a: Vector3D
@@ -11,15 +9,16 @@ struct Block {
   }
 }
 
+let costs = Vector3D(3, 1, 0)
 let blocks = fileContent.split(separator: "\n\n").compactMap { s in
   s.split(separator: "\n")
 }.compactMap(parseBlock)
 
 func part1() -> Int64 {
-  blocks.compactMap(solveBlock).filter { (buttonA, buttonB) in
-    buttonA <= 100 && buttonB <= 100
-  }.compactMap { (buttonA, buttonB) in
-    buttonA * 3 + buttonB
+  blocks.compactMap(solveBlock).filter { buttons in
+    buttons.x <= 100 && buttons.y <= 100
+  }.compactMap { buttons in
+    dotProduct(buttons, costs)
   }.reduce(0, +)
 }
 
@@ -30,56 +29,29 @@ func part2() -> Int64 {
     block.a &+= offset
     block.b &+= offset
     return block
-  }.compactMap(solveBlock).compactMap { (buttonA, buttonB) in
-    buttonA * 3 + buttonB
-  }.reduce(0, +)
+  }.compactMap(solveBlock)
+    .compactMap { buttons in
+      dotProduct(buttons, costs)
+    }.reduce(0, +)
 }
 
 func parseBlock(block: [Substring]) -> Block {
-  let regex = Regex {
-    Capture {
-      ChoiceOf {
-        "Button A"
-        "Button B"
-        "Prize"
-      }
-    }
-    ": "
-    ChoiceOf {
-      "X+"
-      "X="
-    }
-    TryCapture {
-      OneOrMore(.digit)
-    } transform: { s in
-      Int64(s)
-    }
-    ", "
-    ChoiceOf {
-      "Y+"
-      "Y="
-    }
-    TryCapture {
-      OneOrMore(.digit)
-    } transform: { s in
-      Int64(s)
-    }
-  }
-
-  return block.compactMap { s in
-    s.firstMatch(of: regex)
+  block.compactMap { s in
+    s.firstMatch(
+      of: /(Button [AB]|Prize): X.(\d+). Y.(\d+)/
+    )
   }.reduce(into: Block()) {
-    block, match in
+    (block, match) in
     switch match.output {
     case (_, "Button A", let ax, let bx):
-      block.a.x = ax
-      block.b.x = bx
+      block.a.x = Int64(ax)!
+      block.b.x = Int64(bx)!
     case (_, "Button B", let ay, let by):
-      block.a.y = ay
-      block.b.y = by
+      block.a.y = Int64(ay)!
+      block.b.y = Int64(by)!
     case (_, "Prize", let az, let bz):
-      block.a.z = az
-      block.b.z = bz
+      block.a.z = Int64(az)!
+      block.b.z = Int64(bz)!
     default: return
     }
   }
@@ -88,13 +60,16 @@ func parseBlock(block: [Substring]) -> Block {
 func shiftProduct(_ a: Vector3D, _ b: Vector3D) -> Vector3D {
   Vector3D(a.y, a.z, a.x) &* Vector3D(b.z, b.x, b.y)
 }
+
 func crossProduct(_ a: Vector3D, _ b: Vector3D) -> Vector3D {
   return shiftProduct(a, b) &- shiftProduct(b, a)
 }
+
 func dotProduct(_ a: Vector3D, _ b: Vector3D) -> Int64 {
   (a &* b).wrappedSum()
 }
-func solveBlock(block: Block) -> (buttonA: Int64, buttonB: Int64)? {
+
+func solveBlock(block: Block) -> Vector3D? {
   let cross = crossProduct(block.a, block.b)
   let buttons = cross / (-cross.z)
 
@@ -104,5 +79,5 @@ func solveBlock(block: Block) -> (buttonA: Int64, buttonB: Int64)? {
   else {
     return nil
   }
-  return (buttonA: buttons.x, buttonB: buttons.y)
+  return buttons
 }
